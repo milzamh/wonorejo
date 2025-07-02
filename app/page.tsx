d29cf12724b6f333267e2c70bbc571a6bf7b51e1
milzamh <milzamhakimayyasi@mail.ugm.ac.id>
2025-07-02 21:58:10 +0700

    Back FE

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,8 @@ import Jelajah from "./_components/Jelajah";
 import Berita from "./_components/Berita";
 import Potensi from "./_components/Potensi";
 
+const STRAPI_API_URL = 'http://localhost:1338';
+
 export default function Home() {
   return (
     <main className="flex flex-col color-bg">
